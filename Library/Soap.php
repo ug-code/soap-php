687d93ee1422c868b04d1d4d1126a0ee6efe3ca5
ug <ugurgunes@windowslive.com>
2018-02-12 11:51:05 +0300

    Create Soap.php

---
+++ Library/Soap.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Library;
+
+error_reporting(-1);
+ini_set('display_errors', 'On');
+ini_set('soap.wsdl_cache_enabled', 0);
+
+class MySoap {
+
+    public function init($username, $password, $url, $method, $payload) {
+
+            try {
+
+                $output = "";
+                // Create new instance for the client
+                $client = new \SoapClient($url, array('trace'    => 1,
+                    'login'    => $username,
+                    'password' => $password
+                ));
+
+                // Construct the payload to be used to invoke the service.
+                // Invoke the service
+                //$response = $client->findItemClass($payload);
+
+                $client->{$method}($payload);
+
+                // Process the response. In this example,  the content is copied to a variable that is displayed as output
+                $dom                     = new \DOMDocument;
+                $dom->preserveWhiteSpace = FALSE;
+                $dom->loadXML($client->__getLastResponse());
+                $dom->formatOutput       = TRUE;
+                $dom->saveXml();
+                $output     = $dom->saveXml();
+            } catch (\Exception $e) {
+                print "Exception: " . $e->getMessage();
+            }
+        return $output;
+    }
+  
+}
